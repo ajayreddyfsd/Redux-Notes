1fbdfffda394492b138daf2cd9c3cb94377b992a
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-08-20 12:41:08 -0400

    complete

--- Redux Saga/categories (using redux saga)/category.action.js
+++ Redux Saga/categories (using redux saga)/category.action.js
@@ -2,7 +2,6 @@
 //? category action - written the thunk way
 //? category action - written the thunk way
 
-
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
@@ -21,24 +20,3 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 //simple action-creator function returning the action-object
 export const fetchCategoriesFailure = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-
-//? this is the redux-thunk function
-//? this is the redux-thunk function
-//? this is the redux-thunk function
-//? this is the redux-thunk function
-//? this is the redux-thunk function
-//! this is where all the magic happens
-// gets all the docs from firestoreDB and also dipatches all 3 action-creators
-// ofcourse the dispatch is made available through the input argument
-export const fetchCategoriesStartAsync = () => {
-  return async (dispatch) => {
-    dispatch(fetchCategoriesStart());
-    try {
-      const categoriesArray = await getCategoriesAndDocuments("categories");
-      dispatch(fetchCategoriesSuccess(categoriesArray));
-    } catch (error) {
-      dispatch(fetchCategoriesFailure(error));
-    }
-  };
-};
-
--- Redux Saga/categories (using redux-thunk)/category.types.js
+++
@@ -1,8 +0,0 @@
-//earlier we are just having one type, that is setCartItems
-//now, anyways we will include it in the action creator and also wanna do things a bit professionally
-
-export const CATEGORIES_ACTION_TYPES = {
-  FETCH_CATEGORIES_START: "category/FETCH_CATEGORIES_START",
-  FETCH_CATEGORIES_SUCCESS: "category/FETCH_CATEGORIES_SUCCESS",
-  FETCH_CATEGORIES_FAILED: "category/FETCH_CATEGORIES_FAILED",
-};
